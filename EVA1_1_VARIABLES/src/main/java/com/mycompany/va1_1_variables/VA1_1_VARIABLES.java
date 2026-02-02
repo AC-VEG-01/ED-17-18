f4e4d5711b7e44ddcf2fd175b97c390cb762a41d
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2026-02-02 17:14:46 -0600

    Add files via upload

---
+++ EVA1_1_VARIABLES/src/main/java/com/mycompany/va1_1_variables/VA1_1_VARIABLES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.va1_1_variables;
+
+/**
+ *
+ * @author Angel Vega
+ */
+public class VA1_1_VARIABLES {
+
+    public static void main(String[] args) {
+        ///Variables locales:
+        ///Cualquier variables declarada en un método.
+       
+        int x=100; // 4 bytes
+        char caracter= 'c'; // 2 Bytes
+        
+        //Variables dinamicas: --> HEAP
+        //Objetos
+    Persona per= new Persona();        
+        
+    }
+}
+
+class Persona{
+    
+    
+}
