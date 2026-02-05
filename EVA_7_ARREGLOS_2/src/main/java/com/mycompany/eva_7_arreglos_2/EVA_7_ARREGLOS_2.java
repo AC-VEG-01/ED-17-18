9e22b12271403be90a9705bde2157714d5c2009b
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2026-02-05 17:52:12 -0600

    Add files via upload

---
+++ EVA_7_ARREGLOS_2/src/main/java/com/mycompany/eva_7_arreglos_2/EVA_7_ARREGLOS_2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_7_arreglos_2;
+
+/**
+ *
+ * @author Angel Vega
+ */
+public class EVA_7_ARREGLOS_2 {
+
+    public static void main(String[] args) {
+        int[] datos= new int[10000000];//---> 4 Gigas
+        
+        for(int i=0; i < datos.length; i++){
+            datos[i]= (int)(Math.random()* 100);
+            
+       
+        
+     
+            
+        }
+    }
+}
