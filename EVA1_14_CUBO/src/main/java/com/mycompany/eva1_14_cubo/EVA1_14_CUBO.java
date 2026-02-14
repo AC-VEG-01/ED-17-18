9a7ee00198a915198070d1817472b313f5c8f24a
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2026-02-13 22:39:49 -0600

    Add files via upload

---
+++ EVA1_14_CUBO/src/main/java/com/mycompany/eva1_14_cubo/EVA1_14_CUBO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_14_cubo;
+
+/**
+ *
+ * @author Angel Vega
+ */
+public class EVA1_14_CUBO {
+
+    public static void main(String[] args) {
+        int [][][] cubo= new int [3][5][2];
+        
+        for(int i=0; i<cubo.length; i++){///Primer dimension.
+            
+            for(int j=0; j<cubo[i].length; j++){
+                
+                for(int k=0; k<cubo[i][j].length; k++){
+                    cubo[i][j][k]=(int) (Math.random()*100);
+                      
+                
+                }
+            }
+        } 
+    }
+}
